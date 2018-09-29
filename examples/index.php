452bc844557bf897bb0933cc1825a02185b1b9a2
Tomasz Tarnawski <tarnawski27@gmail.com>
2018-09-29 20:56:47 +0200

    Update examples

--- examples/index.php
+++ examples/index.php
@@ -7,11 +7,8 @@ require_once '../vendor/autoload.php';
 
 $client = new Client();
 $associator = new Associator($client);
-//$associator->setApiKey('bf357212-41d4-41de-a0a5-01372c939583');
+$associator->setApiKey('bf357212-41d4-41de-a0a5-01372c939583');
 
-//$result = $associator->createApplication('test', '07fb7006-773a-4c4e-82dd-6f9dc05cbaeb');
-$result = $associator->saveTransaction([1,2]);
-//$result = $associator->getAssociations([2], 1, 1);
-//$result = $associator->importTransactions([[7,14],[9],[3,8]]);
+$result = $associator->getAssociations([2], 0.5, 0.5);
 
 var_dump($result);exit;
