fedf0e49ebe4dbeb190d55398addb35bc467c9dd
Tomasz Tarnawski <tarnawski27@gmail.com>
2018-11-11 18:04:35 +0100

    Add static analytics tools

--- src/Associator/ClientInterface.php
+++ src/Associator/ClientInterface.php
@@ -4,5 +4,11 @@ namespace Associator;
 
 interface ClientInterface
 {
+    /**
+     * @param string $url
+     * @param string $method
+     * @param array $data
+     * @return mixed
+     */
     public function request($url, $method = '', $data = []);
 }
